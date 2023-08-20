e2bb02c474c85cc5a72803cd37f14c7b03c96dd5
zhaix17 <122258771+zhaix17@users.noreply.github.com>
2023-08-20 23:17:57 +0800

    Create random.js

---
+++ random.js
@@ -0,0 +1,19 @@
+const min = 1; // Minimum value for the random number
+const max = 100; // Maximum value for the random number
+
+// Get the user's input
+const numTimes = prompt("Enter a number:");
+
+// Convert the user's input to a number
+const numberOfTimes = parseInt(numTimes);
+
+// Generate a random number within the specified range
+const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Check if the random number is even
+if (randomNumber % 2 === 0) {
+        alert("i love you");
+} else {
+    // Display "imsorry"
+    alert("i don't like you");
+}
